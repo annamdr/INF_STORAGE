f2d18670f0fd8e071e2ea861fa5d56f7d7a342cd
annamdr <annamadrecka@gmail.com>
2018-01-14 08:29:00 +0100

    Lista2

---
+++ Lista2/zad1a_lista2.java
@@ -0,0 +1,18 @@
+﻿/*
+LISTA NR 2
+13.01.2018
+Zad.1a
+*/
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ciag[] = { 1, 3, 5, 7, 9, 11, 13, 15 };
+
+		int suma = 0;
+		for (int k = 0; k < ciag.length; k++)
+			suma = suma + ciag[k];
+		System.out.println("Suma wszystkich liczb ciągu wynosi " +suma);
+
+	}
+}
